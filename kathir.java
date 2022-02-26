6010765c9ebe9d68aab7a7d21faba709a4f46f91
kathirvelrajabalachander <99076316+kathirvelrajabalachander@users.noreply.github.com>
2022-02-26 15:59:12 +0530

    Update kathir.java

--- kathir.java
+++ kathir.java
@@ -6,13 +6,13 @@ class Kathir{
         String str = sc.nextLine();
         String str1 =sc.nextLine();
             String words[]=str.split("\\s");  
-    String capitalizeWord="";  
-    for(String w:words){  
-        String first=w.substring(0,1);  
-        String afterfirst=w.substring(1);  
-        capitalizeWord+=first.toUpperCase()+afterfirst.toLowerCase()+" ";
+            String capitalizeWord="";  
+           for(String w:words){  
+           String first=w.substring(0,1);  
+           String afterfirst=w.substring(1);  
+           capitalizeWord+=first.toUpperCase()+afterfirst.toLowerCase()+" ";
         }
-        System.out.println(capitalizeWord.trim()+" "+ str1.toUpperCase());  
+           System.out.println(capitalizeWord.trim()+" "+ str1.toUpperCase());  
     }  
     
 } 
